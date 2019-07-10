98ab3e97212ec76f1f250b0fa5b1dbb706db4f04
pwirth <wirth@hallowelt.biz>
2019-07-10 16:18:52 +0200

    Add excludes for external file index. ERM:15784
    => Requires: I5314dd589ee236be3a1071c238fecd181b8942ea
    => Needs cherry-pick to REL1_31_dev and REL1_31
    
    Change-Id: I0701c10a072c8405a398d0f953595f067c7a3b3f

---
+++ src/ConfigDefinition/ExternalFilePathsExcludes.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace BS\ExtendedSearch\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition;
+use BlueSpice\Html\FormField\KeyValueField;
+use Exception;
+
+class ExternalFilePathsExcludes extends ConfigDefinition {
+	const EXTENSION_EXTENDED_SEARCH = 'BlueSpiceExtendedSearch';
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_SEARCH . '/' . static::EXTENSION_EXTENDED_SEARCH,
+			static::MAIN_PATH_EXTENSION . '/' . static::EXTENSION_EXTENDED_SEARCH . '/' . static::FEATURE_SEARCH,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/' . static::EXTENSION_EXTENDED_SEARCH,
+		];
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return 'bs-extendedsearch-pref-external-file-paths-excludes';
+	}
+
+	/**
+	 *
+	 * @return KeyValueField
+	 */
+	public function getHtmlFormField() {
+		return new KeyValueField( $this->makeFormFieldParams() );
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	protected function makeFormFieldParams() {
+		return array_merge( parent::makeFormFieldParams(), [
+			'allowAdditions' => true,
+			'valueRequired' => true,
+			'labelsOnlyOnFirst' => true,
+			'keyLabel' => $this->msg(
+				'bs-extendedsearch-pref-external-file-paths-path'
+			)->plain(),
+			'valueLabel' => $this->msg(
+				'bs-extendedsearch-pref-external-file-paths-excludes-exclude'
+			)->plain(),
+			'keyHelp' => $this->msg(
+				'bs-extendedsearch-pref-external-file-paths-path-help'
+			)->plain(),
+			'valueHelp' => $this->msg(
+				'bs-extendedsearch-pref-external-file-paths-excludes-exclude-help'
+			)->plain(),
+			'validation-callback' => function ( $value, $alldata, $parent ) {
+				foreach ( $value as $key => $pattern ) {
+					try {
+						$valid = preg_match( $pattern, null );
+					} catch ( Exception $e ) {
+						return $e->getMessage();
+					}
+					if ( $valid !== false ) {
+						return true;
+					}
+					return $this->msg(
+						'bs-extendedsearch-pref-external-file-paths-excludes-invalid'
+					);
+				}
+				return true;
+			},
+		] );
+	}
+
+}
