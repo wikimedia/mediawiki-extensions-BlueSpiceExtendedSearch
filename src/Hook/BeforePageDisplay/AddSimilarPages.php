2196405edb02a1cc40fc48d715f07178e1a68b85
rvogel <vogel@hallowelt.biz>
2019-08-06 13:37:53 +0200

    Catch exception in "similar pages"
    The "similar pages" functionality is of little importance and should not crash
    the wiki entirely if the search service is not available.
    
    This will also fix CI issues like https://integration.wikimedia.org/ci/job/quibble-composer-mysql-php72-docker/2653/console
    
    NEEDS CHERRY-PICK TO `REL1_31` AND `REL1_31_dev`!
    
    Change-Id: Ia9cb4acf578574a8e592687023c77f6861e389fc

--- src/Hook/BeforePageDisplay/AddSimilarPages.php
+++ src/Hook/BeforePageDisplay/AddSimilarPages.php
@@ -4,6 +4,7 @@ namespace BS\ExtendedSearch\Hook\BeforePageDisplay;
 
 use BlueSpice\Services;
 use BS\ExtendedSearch\Backend;
+use Exception;
 
 class AddSimilarPages extends \BlueSpice\Hook\BeforePageDisplay {
 	protected function skipProcessing() {
@@ -20,25 +21,33 @@ class AddSimilarPages extends \BlueSpice\Hook\BeforePageDisplay {
 	}
 
 	protected function doProcess() {
-		// Execution time for entire code here,
-		// on ~1000 pages index => 102ms
-		$similarPages = Backend::instance()->getSimilarPages(
-			$this->out->getTitle()
-		);
+		try {
+			// Execution time for entire code here,
+			// on ~1000 pages index => 102ms
+			$similarPages = Backend::instance()->getSimilarPages(
+				$this->out->getTitle()
+			);
 
-		$linkRenderer = Services::getInstance()->getLinkRenderer();
+			$linkRenderer = Services::getInstance()->getLinkRenderer();
 
-		$pageLinks = [];
-		foreach ( $similarPages as $title ) {
-			$pageLinks[] = [
-				'page_anchor' => $linkRenderer->makeLink( $title, $title->getText() ),
-				'class' => 'pills'
-			];
+			$pageLinks = [];
+			foreach ( $similarPages as $title ) {
+				$pageLinks[] = [
+					'page_anchor' => $linkRenderer->makeLink(
+						$title,
+						$title->getText()
+					),
+					'class' => 'pills'
+				];
+			}
+
+			$pageLinks = \FormatJson::encode( $pageLinks );
+
+			$this->out->addJsConfigVars( 'bsgESSimilarPages', $pageLinks );
 		}
-
-		$pageLinks = \FormatJson::encode( $pageLinks );
-
-		$this->out->addJsConfigVars( 'bsgESSimilarPages', $pageLinks );
+		catch ( Exception $e ) {
+			wfDebugLog( 'BSExtendedSearch', "AddSimilarPages: {$e->getMessage()}" );
+		}
 	}
 
 }
