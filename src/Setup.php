b7885fd8974a15a680970740ea9708a2d94febd2
dvogel <daniel.vogel@hallowelt.com>
2019-09-06 15:06:52 +0200

    Add screenreader compatibility for searchbox
    [3.1]
    
    Please cherry-pick to master
    
    Change-Id: Icf4f70bd4b8eb64459abb699b18be4da444bf129

--- src/Setup.php
+++ src/Setup.php
@@ -60,7 +60,8 @@ class Setup {
 			[
 				'id' => 'bs-extendedsearch-input',
 				'type' => 'text',
-				'name' => 'raw_term'
+				'name' => 'raw_term',
+				'aria-label' => wfMessage( 'bs-extendedsearch-search-input-aria-label' )->plain()
 			]
 		);
 
@@ -72,7 +73,8 @@ class Setup {
 			[
 				'id' => 'bs-extendedsearch-mobile-input',
 				'type' => 'text',
-				'name' => 'raw_term'
+				'name' => 'raw_term',
+				'aria-label' => wfMessage( 'bs-extendedsearch-search-input-aria-label' )->plain()
 			]
 		);
 
