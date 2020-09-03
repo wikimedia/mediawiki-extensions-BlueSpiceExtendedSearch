cea09247fb1977f061b3a81a1b1299be4cc645bb
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-09-03 09:18:38 +0200

    Make searching in raw wikitext configurable
    Requires "wikipage" index to be re-inited and rebuilt
    By default set to true => no change
    
    [3.1.x]
    
    ERM 20723
    
    Change-Id: Ia5afcf76341bc5819ea1284ad3a0e23fd44c5d0e

--- src/Source/LookupModifier/WikiPageQSSourceFields.php
+++ src/Source/LookupModifier/WikiPageQSSourceFields.php
@@ -2,6 +2,8 @@
 
 namespace BS\ExtendedSearch\Source\LookupModifier;
 
+use MediaWiki\MediaWikiServices;
+
 class WikiPageQSSourceFields extends Base {
 
 	/**
@@ -10,7 +12,7 @@ class WikiPageQSSourceFields extends Base {
 	public function apply() {
 		$queryString = $this->oLookup->getQueryString();
 
-		$fields = [ 'rendered_content', 'prefixed_title', 'display_title^2' ];
+		$fields = $this->getFields();
 		if ( isset( $queryString['fields'] ) && is_array( $queryString['fields'] ) ) {
 			$queryString['fields'] = array_merge( $queryString['fields'], $fields );
 		} else {
@@ -24,10 +26,29 @@ class WikiPageQSSourceFields extends Base {
 		$queryString = $this->oLookup->getQueryString();
 
 		if ( isset( $queryString['fields'] ) && is_array( $queryString['fields'] ) ) {
-			$queryString['fields'] = array_diff( $queryString['fields'], [ 'rendered_content', 'prefixed_title', 'display_title^2' ] );
+			$queryString['fields'] = array_diff(
+				$queryString['fields'],
+				$this->getFields()
+			);
 		}
 
 		$this->oLookup->setQueryString( $queryString );
 	}
 
+	/**
+	 * @return string[]
+	 */
+	private function getFields() {
+		$fields = [ 'rendered_content', 'prefixed_title', 'display_title^2' ];
+		if ( $this->shouldSearchInRaw() ) {
+			$fields[] = 'source_content';
+		}
+
+		return $fields;
+	}
+
+	private function shouldSearchInRaw() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		return (bool)$config->get( 'ESSearchInRawWikitext' );
+	}
 }
