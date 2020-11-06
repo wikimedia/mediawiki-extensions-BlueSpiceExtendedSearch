0f43b858eeced02a623e2e534db8279d157d8c82
Andrii Shykov <banderlog17@gmail.com>
2020-11-06 13:45:58 +0200

    Add quotes to date patterns
    [3.1.x]
    
    ERM19805
    
    Change-Id: I32b53b70293021a7686e2fdccce23aca03681465

---
+++ src/Source/LookupModifier/RegExpQuoter.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace BS\ExtendedSearch\Source\LookupModifier;
+
+use MediaWiki\MediaWikiServices;
+
+class RegExpQuoter extends Base {
+
+	/**
+	 * @var array
+	 */
+	protected $queryString;
+
+	/**
+	 * @var string
+	 */
+	protected $originalQuery;
+
+	/**
+	 *
+	 * @return void
+	 */
+	public function apply() {
+		$this->queryString = $this->oLookup->getQueryString();
+		$this->originalQuery = $this->queryString['query'];
+
+		/*
+		 * If search query matches one of the date patterns:
+		 * we will quote it.
+		 * */
+		$patterns = $this->getPatterns();
+		$this->queryString['query'] = $this->quoteQueryByPattern( $this->queryString['query'], $patterns );
+		$this->oLookup->setQueryString( $this->queryString );
+	}
+
+	/**
+	 *
+	 * @return void
+	 */
+	public function undo() {
+		$this->queryString = $this->oLookup->getQueryString();
+		$this->queryString['query'] = $this->originalQuery;
+		$this->oLookup->setQueryString( $this->queryString );
+	}
+
+	/**
+	 *
+	 * @return BS\ExtendedSearch\Lookup
+	 */
+	public function getLookup() {
+		return $this->oLookup;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getPriority() {
+		return 51;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	protected function getPatterns() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'bsg' );
+		if ( $config->has( 'ESLookupModifierRegExPatterns' ) ) {
+			return $config->get( 'ESLookupModifierRegExPatterns' );
+		}
+		return [];
+	}
+
+	/**
+	 * @param string $query
+	 * @param array $pcrePatterns
+	 * @return string
+	 */
+	protected function quoteQueryByPattern( $query, $pcrePatterns ) {
+		$replaceCounter = 0;
+		$mapping = [];
+
+		foreach ( $pcrePatterns as $singlePattern ) {
+
+			if ( preg_match_all( "/$singlePattern/", $query, $m1 ) ) {
+
+				if ( !count( $m1 ) ) {
+					continue;
+				}
+				$m2 = $m1[0];
+				$m2 = array_unique( $m2 );
+
+				foreach ( $m2 as $k => $matchedPart ) {
+					$replaceCounter++;
+					$placeholder = "###Q$replaceCounter###";
+					$mapping[$placeholder] = $matchedPart;
+					$query = str_replace( $matchedPart, $placeholder, $query );
+				}
+
+			}
+		}
+		foreach ( $mapping as $k => $v ) {
+			$query = str_replace( $k, '"' . $v . '"', $query );
+		}
+
+		return $query;
+	}
+
+}
---
+++ tests/phpunit/Source/LookupModifier/RegExpQuoterTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace BS\ExtendedSearch\Tests\Source\LookupModifier;
+
+use BS\ExtendedSearch\Lookup;
+use BS\ExtendedSearch\Source\LookupModifier\RegExpQuoter;
+use MediaWikiTestCase;
+
+class RegExpQuoterTest extends MediaWikiTestCase {
+
+	/**
+	 * @dataProvider provideTestQueries
+	 * @covers BS\ExtendedSearch\Source\LookupModifier\RegExpQuoter::apply
+	 */
+	public function testApply( $singleQuery, $singleQueryQuoted ) {
+		$context = $this->createMock( 'IContextSource' );
+
+		$lookup = new Lookup();
+		$lookup->setQueryString( $singleQuery );
+
+		$regExpQuoter = new RegExpQuoter( $lookup, $context );
+		$regExpQuoter->apply();
+
+		$query = $lookup->getQueryString();
+		$this->assertEquals( $singleQueryQuoted, $query['query'] );
+	}
+
+	/**
+	 * @dataProvider provideTestQueries
+	 * @covers BS\ExtendedSearch\Source\LookupModifier\RegExpQuoter::undo
+	 */
+	public function testUndo( $singleQuery, $singleQueryQuoted ) {
+		$context = $this->createMock( 'IContextSource' );
+
+		$lookup = new Lookup();
+		$lookup->setQueryString( $singleQuery );
+
+		$regExpQuoter = new RegExpQuoter( $lookup, $context );
+		$regExpQuoter->apply();
+
+		$regExpQuoter->undo();
+
+		$query = $lookup->getQueryString();
+		$this->assertEquals( $singleQuery, $query['query'] );
+	}
+
+	public function provideTestQueries() {
+		$baseTestQueries = [
+			[ '19-02-2006', '"19-02-2006"' ],
+			[ '15-02', '"15-02"' ],
+			[ '19-02-2007 www sss 19-02', '"19-02-2007" www sss "19-02"' ],
+			[ '19-02-2007 19-02 19-02-2008 2008-19-02 19-02', '"19-02-2007" "19-02" "19-02-2008" "2008-19-02" "19-02"' ],
+			[ '10-20, привет  Å Ä 2001-10-1, 2001-1-1, 1-2001-1, 1-1-10', '"10-20", привет  Å Ä "2001-10-1", "2001-1-1", "1-2001-1", "1-1-10"' ],
+			[ '10-20, привет  Å Ä 2001-10-1, 20-10, 汉字', '"10-20", привет  Å Ä "2001-10-1", "20-10", 汉字' ],
+		];
+
+		$delimiters = [ '/', '.', '\\' ];
+		$testQueries = [];
+		foreach ( $delimiters as $delimiter ) {
+			$modBaseTestQueries = [];
+			foreach ( $baseTestQueries as $queryPair ) {
+				$modBaseTestQueries[] = [
+					str_replace( '-', $delimiter, $queryPair[0] ),
+					str_replace( '-', $delimiter, $queryPair[1] ),
+				];
+			}
+			$testQueries = array_merge( $modBaseTestQueries, $testQueries );
+		}
+		$testQueries = array_merge( $baseTestQueries, $testQueries );
+
+		return $testQueries;
+	}
+
+}
