b49cd81655ba644ae8339406ae45003f5709f610
d.savuljesku <savuljesku@hallowelt.com>
2021-04-30 13:09:35 +0200

    Add "namespaces" as a param for tagsearch
    for some reason, as with categories, both "ns" and "namespace" attributes
    are in use. So we make both work
    
    [3.2.x]
    
    ERM 23862
    
    Change-Id: Idee5d1f130c6362f57202ef5fca9bc8dc20f5966

--- src/Tag/TagSearch.php
+++ src/Tag/TagSearch.php
@@ -73,6 +73,12 @@ class TagSearch extends Tag {
 			[]
 		);
 		$namespaceListParam->setArrayValues( [ 'hastoexist' => true ] );
+		$namespaceListParamFull = new BSNamespaceListParam(
+			ParamType::NAMESPACE_LIST,
+			static::PARAM_NAMESPACE_FULLNAME,
+			[]
+		);
+		$namespaceListParam->setArrayValues( [ 'hastoexist' => true ] );
 
 		$categoryParam  = new BSCategoryListParam(
 			ParamType::CATEGORY_LIST, static::PARAM_CATEGORY_FULLNAME, []
@@ -87,6 +93,7 @@ class TagSearch extends Tag {
 				static::PARAM_TYPE
 			),
 			$namespaceListParam,
+			$namespaceListParamFull,
 			$catParam,
 			$categoryParam,
 			new ParamDefinition(
