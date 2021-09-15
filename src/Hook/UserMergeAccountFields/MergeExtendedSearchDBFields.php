5e25c2d69fbf78ee7911f8c1d43e333d24f65e9c
pwirth <wirth@hallowelt.biz>
2021-09-15 14:25:27 +0200

    Add handler for UserMerge
    ERM:16839
    [3.2.x]
    
    Depends-On: Icc2c1449c7ab2c4dabc6976dac384136c0c069cc
    Change-Id: I88f90eb23fedc9d04df9e27fac6a071f68c5a72f

---
+++ src/Hook/UserMergeAccountFields/MergeExtendedSearchDBFields.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BS\ExtendedSearch\Hook\UserMergeAccountFields;
+
+use BlueSpice\DistributionConnector\Hook\UserMergeAccountFields;
+
+class MergeExtendedSearchDBFields extends UserMergeAccountFields {
+
+	protected function doProcess() {
+		$this->updateFields[] = [ 'bs_extendedsearch_relevance', 'esr_user' ];
+		$this->updateFields[] = [ 'bs_extendedsearch_history', 'esh_id' ];
+	}
+
+}
