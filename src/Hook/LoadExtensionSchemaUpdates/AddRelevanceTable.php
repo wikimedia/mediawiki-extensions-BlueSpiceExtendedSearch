da9b3517d0a974118853a3f8d567a1061008b0a1
pavloyashchenko <pyashchenko@hallowelt.com>
2022-08-04 14:47:04 +0200

    Create AbstractSchema for sql table
    Create Json file; Generate 3 files(sqlite, postgres, mysql); Change path in addExtensionTable() to generated file.
    
    ERM29227
    
    [4.2.x]
    
    Change-Id: I563bcd7b551fe1fe0e68c1f2161e6340d91e0e54

--- src/Hook/LoadExtensionSchemaUpdates/AddRelevanceTable.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddRelevanceTable.php
@@ -6,17 +6,35 @@ use BlueSpice\Hook\LoadExtensionSchemaUpdates;
 
 class AddRelevanceTable extends LoadExtensionSchemaUpdates {
 	protected function doProcess() {
-		$dir = $this->getExtensionPath() . '/maintenance/db';
+		$dbType = $this->updater->getDB()->getType();
+		$dir = $this->getExtensionPath();
 
 		$this->updater->addExtensionTable(
 			'bs_extendedsearch_relevance',
-			"$dir/bs_extendedsearch_relevance.sql"
+			"$dir/maintenance/db/sql/$dbType/bs_extendedsearch_relevance-generated.sql"
 		);
 
-		$this->updater->modifyExtensionField( 'bs_extendedsearch_relevance', 'rel_user', "$dir/bs_extendedsearch_relevance.patch.user.sql" );
-		$this->updater->modifyExtensionField( 'bs_extendedsearch_relevance', 'rel_result', "$dir/bs_extendedsearch_relevance.patch.result.sql" );
-		$this->updater->modifyExtensionField( 'bs_extendedsearch_relevance', 'rel_value', "$dir/bs_extendedsearch_relevance.patch.value.sql" );
-		$this->updater->modifyExtensionField( 'bs_extendedsearch_relevance', 'rel_timestamp', "$dir/bs_extendedsearch_relevance.patch.timestamp.sql" );
+		if ( $dbType == 'mysql' ) {
+			$this->updater->modifyExtensionField(
+				'bs_extendedsearch_relevance',
+				'rel_user',
+				"$dir/maintenance/db/bs_extendedsearch_relevance.patch.user.sql"
+			);
+			$this->updater->modifyExtensionField(
+				'bs_extendedsearch_relevance',
+				'rel_result',
+				"$dir/maintenance/db/bs_extendedsearch_relevance.patch.result.sql" );
+			$this->updater->modifyExtensionField(
+				'bs_extendedsearch_relevance',
+				'rel_value',
+				"$dir/maintenance/db/bs_extendedsearch_relevance.patch.value.sql"
+			);
+			$this->updater->modifyExtensionField(
+				'bs_extendedsearch_relevance',
+				'rel_timestamp',
+				"$dir/maintenance/db/bs_extendedsearch_relevance.patch.timestamp.sql"
+			);
+		}
 	}
 
 	/**
