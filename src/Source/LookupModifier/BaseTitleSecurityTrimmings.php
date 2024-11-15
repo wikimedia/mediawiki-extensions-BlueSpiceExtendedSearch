ebc0cab5820404838b7fca1909971d8075a655f2
d.savuljesku <d.savuljesku@gmail.com>
2024-11-15 11:47:34 +0100

    Fix logic for title permission checking
    It would run for hundreds of times in case of a big shared index
    
    [4.5.x]
    
    ERM34323
    
    Change-Id: I02b5f1c66c7d24afb0f48f0affcd11a988a5b0e0
    (cherry picked from commit 08d4ff8fd18620a347ef8fdf133c9042ce7c1880)
    (cherry picked from commit 60a4a56d6d7507da2780eaaee632ee2fb171f0dd)
    (cherry picked from commit e7b448bf6ba83d89c43c703eb53f939a8ac02216)
    (cherry picked from commit 505a76325cba8db15cab0aca51741dbe4e40cfaf)

--- src/Source/LookupModifier/BaseTitleSecurityTrimmings.php
+++ src/Source/LookupModifier/BaseTitleSecurityTrimmings.php
@@ -118,7 +118,7 @@ class BaseTitleSecurityTrimmings extends LookupModifier {
 				if ( isset( $data['prefixed_title'] ) ) {
 					$title = Title::newFromText( $data['prefixed_title'] );
 				} else {
-					$title = Title::makeTitle( $data['namespace'], $data['basename'] );
+					$title = Title::makeTitleSafe( $data['namespace'], $data['basename'] );
 				}
 				if ( !$title instanceof Title ) {
 					if ( $title->isContentPage() && $title->exists() == false ) {
@@ -130,7 +130,8 @@ class BaseTitleSecurityTrimmings extends LookupModifier {
 
 				if ( $title->isSpecialPage() ) {
 					$sp = $spFactory->getPage( $title->getDBkey() );
-					if ( !$sp instanceof SpecialPage ) {
+					if ( !( $sp instanceof SpecialPage ) ) {
+
 						$excludes[] = $resultObject->getId();
 						continue;
 					}
@@ -142,10 +143,9 @@ class BaseTitleSecurityTrimmings extends LookupModifier {
 						$excludes[] = $resultObject->getId();
 						continue;
 					}
-				}
-
-				if ( !$permManager->userCan( 'read', $user, $title ) ) {
+				} elseif ( !$permManager->userCan( 'read', $user, $title ) ) {
 					$excludes[] = $resultObject->getId();
+					continue;
 				}
 
 				$validCount++;
