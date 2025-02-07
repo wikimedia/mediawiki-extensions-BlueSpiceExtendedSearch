e3a8591a0cff8c4c5629bbe08c442d1849de9683
d.savuljesku <d.savuljesku@gmail.com>
2025-02-07 10:13:02 +0100

    Integrate into PDFCreator
    [5.0]
    
    ERM38759
    
    Change-Id: I413d417bf209a3fac7ed3785a8fcb29b7c07bdfe
    (cherry picked from commit 42887bddd6e78f8463d4dd08a85edec519ea6a0f)

---
+++ src/Integration/PDFCreator/Processor/RemoveTagSearch.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BS\ExtendedSearch\Integration\PDFCreator\Processor;
+
+use DOMXPath;
+use MediaWiki\Extension\PDFCreator\IProcessor;
+use MediaWiki\Extension\PDFCreator\Utility\ExportContext;
+use MediaWiki\Extension\PDFCreator\Utility\ExportPage;
+
+class RemoveTagSearch implements IProcessor {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute(
+		array &$pages, array &$images, array &$attachments, ExportContext $context, string $module = '', $params = []
+	): void {
+		/** @var ExportPage $page */
+		foreach ( $pages as &$page ) {
+			$dom = $page->getDOMDocument();
+			$xpath = new DOMXPath( $dom );
+			$forms = $xpath->query( "//*[contains(@class, 'bs-tagsearch-form')]" );
+			foreach ( $forms as $form ) {
+				$form->parentNode->removeChild( $form );
+			}
+		}
+	}
+}
